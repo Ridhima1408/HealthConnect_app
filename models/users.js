b94b3b5822d04a7c650c474837c98c421f2baf1d
RidhimaSolet <soletridhima8@gmail.com>
2025-08-18 13:09:45 +0530

    Changes

--- models/users.js
+++ models/users.js
@@ -1,11 +1,10 @@
-// models/User.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email:    { type: String, required: true, unique: true }, // <-- add this
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role:     { type: String, default: "user" }
+  role: { type: String, enum: ["user", "admin"], default: "user" }
 });
 
 module.exports = mongoose.model("User", userSchema);
