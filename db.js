f971695129aa6c7141a34e653d82f2c5b321037d
RidhimaSolet <soletridhima8@gmail.com>
2025-08-18 08:41:03 +0530

    Changes

--- db.js
+++ db.js
@@ -10,3 +10,12 @@ db.connect((err) => {
   console.log('Connected to MySQL');
 });
 module.exports = db;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/healthconnect", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected via Docker"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
+
