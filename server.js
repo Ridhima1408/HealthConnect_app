66bd648415ebee4f5548a645100b52ad10e744bd
RidhimaSolet <soletridhima8@gmail.com>
2025-08-18 10:40:46 +0530

    Changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const fs = require('fs');
 
 const app = express();
