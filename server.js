2ba670146f7c36a5529512dbd54943a5b235ec96
Ridhima Solet <soletridhima8@gmail.com>
2025-08-19 10:12:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -183,10 +183,10 @@ app.get('/test', (req, res) => {
 });
 
 // Start Server
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
-// });
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
+});
 
-app.listen(PORT, () => {
- console.log(`✅ Server running on http://localhost:${PORT}`);
- });
+// app.listen(PORT, () => {
+//  console.log(`✅ Server running on http://localhost:${PORT}`);
+//  });
